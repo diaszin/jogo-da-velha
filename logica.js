91dd49344ade57f481cd8a9cfb79fb407ae8019d
diaszin <kaiquedias2013@gmail.com>
2022-10-21 15:43:32 -0300

    Adicionando formas de ganhar o jogo, criando a lógica de vencer

--- logica.js
+++ logica.js
@@ -1,16 +1,16 @@
 const campos = document.getElementsByClassName("campo")
 
-function InserirSimbolo(){
+function InserirSimbolo() {
     var ultimoSimboloInserido = []
     var jogadas = 0
 
-    for(let i =0;i<campos.length;i++){
-        campos[i].addEventListener("click", ()=>{
-            if(jogadas <=9){
-                if(ultimoSimboloInserido[ultimoSimboloInserido.length-1] == "X"){
+    for (let i = 0; i < campos.length; i++) {
+        campos[i].addEventListener("click", () => {
+            if (jogadas <= 9) {
+                if (ultimoSimboloInserido[ultimoSimboloInserido.length - 1] == "X") {
                     campos[i].innerHTML = "O"
                     ultimoSimboloInserido.push("O")
-                }else{
+                } else {
                     campos[i].innerHTML = "X"
                     ultimoSimboloInserido.push("X")
                 }
@@ -24,11 +24,50 @@ function InserirSimbolo(){
 }
 
 
-function VerificarSeGanhou(jogadas){
-    if(jogadas >=5){
-        if((campos[0].innerHTML=="X" && campos[1].innerHTML=="X" && campos[2].innerHTML=="X") || (campos[0].innerHTML=="O" && campos[1].innerHTML=="O" && campos[2].innerHTML=="O")){
+function VerificarSeGanhou(jogadas) {
+    if (jogadas >= 5) {
+        // Primeira linha
+        if ((campos[0].innerHTML == "X" && campos[1].innerHTML == "X" && campos[2].innerHTML == "X") || (campos[0].innerHTML == "O" && campos[1].innerHTML == "O" && campos[2].innerHTML == "O")) {
             alert("Você Ganhou !")
-            location.reload()
+
+        }
+        //Segunda Linha
+        if ((campos[3].innerHTML == "X" && campos[4].innerHTML == "X" && campos[5].innerHTML == "X") || (campos[3].innerHTML == "O" && campos[4].innerHTML == "O" && campos[5].innerHTML == "O")) {
+            alert("Você Ganhou !")
+
+        }
+        //Terceira Linha
+        if ((campos[6].innerHTML == "X" && campos[7].innerHTML == "X" && campos[8].innerHTML == "X") || (campos[6].innerHTML == "O" && campos[7].innerHTML == "O" && campos[8].innerHTML == "O")) {
+            alert("Você Ganhou !")
+
+        }
+        //Primeira Coluna
+        if ((campos[0].innerHTML == "X" && campos[3].innerHTML == "X" && campos[6].innerHTML == "X") || (campos[0].innerHTML == "O" && campos[3].innerHTML == "O" && campos[6].innerHTML == "O")) {
+            alert("Você Ganhou !")
+
+        }
+        //Segunda Coluna
+        if ((campos[1].innerHTML == "X" && campos[4].innerHTML == "X" && campos[7].innerHTML == "X") || (campos[1].innerHTML == "O" && campos[4].innerHTML == "O" && campos[7].innerHTML == "O")) {
+            alert("Você Ganhou !")
+
+        }
+        //Terceira Coluna
+        if ((campos[2].innerHTML == "X" && campos[5].innerHTML == "X" && campos[8].innerHTML == "X") || (campos[2].innerHTML == "O" && campos[5].innerHTML == "O" && campos[8].innerHTML == "O")) {
+            alert("Você Ganhou !")
+
+        }
+        //Diagonal da Esquerda pra direita
+        if ((campos[0].innerHTML == "X" && campos[4].innerHTML == "X" && campos[8].innerHTML == "X") || (campos[0].innerHTML == "O" && campos[4].innerHTML == "O" && campos[8].innerHTML == "O")) {
+            alert("Você Ganhou !")
+
+        }
+        //Diagonal da Direita pra Esquerda
+        if ((campos[2].innerHTML == "X" && campos[4].innerHTML == "X" && campos[6].innerHTML == "X") || (campos[2].innerHTML == "O" && campos[4].innerHTML == "O" && campos[6].innerHTML == "O")) {
+            alert("Você Ganhou !")
+
+        }
+        else {
+            alert('Deu empate !')
         }
     }
 }
