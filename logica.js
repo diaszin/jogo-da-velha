2c62dbf2b0e54c7dcd95670e213bbfac186cac12
diaszin <kaiquedias2013@gmail.com>
2022-11-04 16:51:29 -0300

    Melhorando a lógica.

--- logica.js
+++ logica.js
@@ -35,56 +35,49 @@ function InserirSimbolo() {
 
 
 function VerificarSeGanhou(jogadas) {
+    var tabuleiro = [
+        [campos[0].innerHTML, campos[1].innerHTML, campos[2].innerHTML],
+        [campos[3].innerHTML, campos[4].innerHTML, campos[5].innerHTML],
+        [campos[6].innerHTML, campos[7].innerHTML, campos[8].innerHTML],
+    ]
     if (jogadas >= 5) {
         // Primeira linha
-        if ((campos[0].innerHTML == "X" && campos[1].innerHTML == "X" && campos[2].innerHTML == "X") || (campos[0].innerHTML == "O" && campos[1].innerHTML == "O" && campos[2].innerHTML == "O")) {
+        if (tabuleiro[0][0] == tabuleiro[0][1] == tabuleiro[0][2]) {
             alert("Você Ganhou !")
             ganhou = true
 
         }
-        //Segunda Linha
-        if ((campos[3].innerHTML == "X" && campos[4].innerHTML == "X" && campos[5].innerHTML == "X") || (campos[3].innerHTML == "O" && campos[4].innerHTML == "O" && campos[5].innerHTML == "O")) {
+        if (tabuleiro[1][0] == tabuleiro[1][1] == tabuleiro[1][2]) {
             alert("Você Ganhou !")
             ganhou = true
 
         }
-        //Terceira Linha
-        if ((campos[6].innerHTML == "X" && campos[7].innerHTML == "X" && campos[8].innerHTML == "X") || (campos[6].innerHTML == "O" && campos[7].innerHTML == "O" && campos[8].innerHTML == "O")) {
+        if (tabuleiro[2][0] == tabuleiro[2][1] == tabuleiro[2][2]) {
             alert("Você Ganhou !")
             ganhou = true
-
+            
         }
-        //Primeira Coluna
-        if ((campos[0].innerHTML == "X" && campos[3].innerHTML == "X" && campos[6].innerHTML == "X") || (campos[0].innerHTML == "O" && campos[3].innerHTML == "O" && campos[6].innerHTML == "O")) {
+        if(tabuleiro[0][0] == tabuleiro[1][0] == tabuleiro[2][0]){
             alert("Você Ganhou !")
             ganhou = true
-
         }
-        //Segunda Coluna
-        if ((campos[1].innerHTML == "X" && campos[4].innerHTML == "X" && campos[7].innerHTML == "X") || (campos[1].innerHTML == "O" && campos[4].innerHTML == "O" && campos[7].innerHTML == "O")) {
+        if(tabuleiro[0][1] == tabuleiro[1][1] == tabuleiro[2][1]){
             alert("Você Ganhou !")
             ganhou = true
-
         }
-        //Terceira Coluna
-        if ((campos[2].innerHTML == "X" && campos[5].innerHTML == "X" && campos[8].innerHTML == "X") || (campos[2].innerHTML == "O" && campos[5].innerHTML == "O" && campos[8].innerHTML == "O")) {
+        if(tabuleiro[0][2] == tabuleiro[1][2] == tabuleiro[2][2]){
             alert("Você Ganhou !")
             ganhou = true
-
         }
-        //Diagonal da Esquerda pra direita
-        if ((campos[0].innerHTML == "X" && campos[4].innerHTML == "X" && campos[8].innerHTML == "X") || (campos[0].innerHTML == "O" && campos[4].innerHTML == "O" && campos[8].innerHTML == "O")) {
+        if(tabuleiro[0][0] == tabuleiro[1][1] == tabuleiro[2][2]){
             alert("Você Ganhou !")
             ganhou = true
-
         }
-        //Diagonal da Direita pra Esquerda
-        if ((campos[2].innerHTML == "X" && campos[4].innerHTML == "X" && campos[6].innerHTML == "X") || (campos[2].innerHTML == "O" && campos[4].innerHTML == "O" && campos[6].innerHTML == "O")) {
+        if(tabuleiro[0][2] == tabuleiro[1][1] == tabuleiro[2][0]){
             alert("Você Ganhou !")
             ganhou = true
-
         }
-        if(jogadas==9){
+        else if(jogadas>=9){
             alert("Deu empate !")
             ganhou = true
         }
